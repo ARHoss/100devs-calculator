51fb9ac0f6e7fc52743deac9cc22b7e277b89568
arhoss <rhossaincse@gmail.com>
2023-01-10 11:05:57 -0700

    js part completed using basic functions

--- js/main.js
+++ js/main.js
@@ -9,12 +9,56 @@ for(let symbol of symbols){
 function calculate(e){
 
     const symbol = document.querySelector(`span[data-key='${e.keyCode}']`);
+    const result = document.querySelector('#mathResult');
 
     if(!symbol){//If other key are pressed return
         return;
     }
 
     symbol.classList.add('mathSymbolClick');
+
+    // Calculation
+    let num1;
+    let op;
+    let num2;
+
+    if(symbol.textContent != '='){
+        result.textContent += symbol.textContent;
+    }else{
+        let resultCal = result.textContent;
+        resultCal = resultCal.split('');
+
+        for (let i = 0; i < resultCal.length; i++) {
+            
+            if(resultCal[i] == '+' ||
+            resultCal[i] == '-' ||
+            resultCal[i] == '/' ||
+            resultCal[i] == 'x'
+            ){
+                op =resultCal[i] == 'x'?'*':resultCal[i];
+                num1 = num1?? +resultCal.splice(0,i).join('');
+                num2 = num2?? +resultCal.splice(1,resultCal.length-1).join('');
+            }
+                        
+        }
+
+        if(op == '+'){
+            result.textContent = num1+num2;
+        }else if(op == '-'){
+            result.textContent = num1-num2;
+        }else if(op == '*'){
+            result.textContent = num1*num2;
+        }else if(op == '/'){
+            result.textContent = num1/num2;
+        }else{
+            result.textContent = NaN;
+        }
+        
+        
+    }
+    
+
+
 
     
 
